9d8d1224e8ca0df308227c9a0e1a73924c4221b6
Colin Hebert <cgabear@gmail.com>
2024-09-30 15:22:46 -0500

    chore: remove testing stuff

--- frontend/app/lockerroom/components/PaginatedList.tsx
+++ frontend/app/lockerroom/components/PaginatedList.tsx
@@ -2,7 +2,14 @@
 import React from "react";
 /* eslint-disable func-style */
 import { useState, useEffect } from "react";
-import { Box, Center, GridItem, SimpleGrid, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+    Box,
+    Center,
+    GridItem,
+    SimpleGrid,
+    Text,
+    useBreakpointValue,
+} from "@chakra-ui/react";
 import TrendingCard from "@/components/trending_now/TrendingCard";
 import TradingCardInfo from "@/hooks/TradingCardInfo";
 import PaginationNavigator from "./PaginationNavigator";
@@ -10,27 +17,27 @@ import { setWithExpiry } from "@/components/localStorageFunctions";
 import SerializedTradingCard from "@/hooks/SerializedTradingCard";
 
 interface ProfilePageProps {
-	currentCard?: TradingCardInfo;
-	setCurrentCard?: (card: TradingCardInfo) => void;
-	setViewCardClicked?: (clicked: boolean) => void;
-	setCurrentSerializedCard?: (card: SerializedTradingCard) => void;
-	tabName?: string;
-	currentUserId?: string;
-	onSendCardModalOpen?: () => void;
-	loginModalOpen?: () => void;
-	onAddToCollectionModalOpen?: () => void;
-	privateView?: boolean;
-  }
+    currentCard?: TradingCardInfo;
+    setCurrentCard?: (card: TradingCardInfo) => void;
+    setViewCardClicked?: (clicked: boolean) => void;
+    setCurrentSerializedCard?: (card: SerializedTradingCard) => void;
+    tabName?: string;
+    currentUserId?: string;
+    onSendCardModalOpen?: () => void;
+    loginModalOpen?: () => void;
+    onAddToCollectionModalOpen?: () => void;
+    privateView?: boolean;
+}
 
-  interface PaginatedListProps {
-	itemsPerPage: number;
-	data: TradingCardInfo[] | SerializedTradingCard[];
-	targetRef: React.RefObject<HTMLDivElement>;
-	currentPage: number;
-	setCurrentPage: (page: number) => void;
-	profilePage?: boolean;
-	profilePageProps?: ProfilePageProps;
-  }
+interface PaginatedListProps {
+    itemsPerPage: number;
+    data: TradingCardInfo[] | SerializedTradingCard[];
+    targetRef: React.RefObject<HTMLDivElement>;
+    currentPage: number;
+    setCurrentPage: (page: number) => void;
+    profilePage?: boolean;
+    profilePageProps?: ProfilePageProps;
+}
 
 /**
  * Generates a paginatied list of TrendingCard components.
@@ -53,132 +60,169 @@ interface ProfilePageProps {
  * @returns {JSX.Element} The paginated list component.
  */
 export default function PaginatedList({
-	itemsPerPage,
-	data,
-	targetRef,
-	currentPage,
-	setCurrentPage,
-	profilePage = false,
-	profilePageProps,
+    itemsPerPage,
+    data,
+    targetRef,
+    currentPage,
+    setCurrentPage,
+    profilePage = false,
+    profilePageProps,
 }: PaginatedListProps) {
+    const [isPageFlipping, setIsPageFlipping] = useState(false);
 
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
-	const [ isPageFlipping, setIsPageFlipping ] = useState(false);
+    const totalPages = Math.ceil(data.length / itemsPerPage);
 
-	const indexOfLastItem = currentPage * itemsPerPage;
-	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-	const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+    const noCardMessageBoxWidth = useBreakpointValue({
+        base: "50vw",
+        md: "30%",
+    });
 
-	const totalPages = Math.ceil(data.length / itemsPerPage);
+    const parentStackRef = React.useRef<HTMLDivElement>(null);
 
-	const noCardMessageBoxWidth = useBreakpointValue({ base:"50vw", md:"30%" });
+    const handleClick = (page: number) => {
+        setIsPageFlipping(true);
+        setCurrentPage(page);
+        setWithExpiry("lastPaginationPage", page.toString(), 24); // Save the page to local storage with a 24 hour expiry
+        setTimeout(() => {
+            setIsPageFlipping(false);
+        }, 1000);
+    };
 
-	const parentStackRef = React.useRef<HTMLDivElement>(null);
+    const isMobile = useBreakpointValue({ base: true, md: false });
 
-	const handleClick = (page: number) => {
-		setIsPageFlipping(true);
-		setCurrentPage(page);
-		setWithExpiry("lastPaginationPage", page.toString(), 24); // Save the page to local storage with a 24 hour expiry
-		setTimeout(() => {
-			setIsPageFlipping(false);
-		}, 1000);
-	};
+    /**
+     * Focuses on the current card when the view card button or the card is clicked.
+     */
+    function focusCurrentCard(card: TradingCardInfo | SerializedTradingCard) {
+        if (
+            profilePageProps &&
+            profilePageProps.setViewCardClicked &&
+            profilePageProps.currentCard
+        ) {
+            // Set the view card clicked to true
+            profilePageProps.setViewCardClicked(true);
+            // If the card is a serialized card, set the current serialized card
+            if (
+                card instanceof SerializedTradingCard &&
+                profilePageProps.setCurrentSerializedCard
+            ) {
+                profilePageProps.setCurrentSerializedCard(card);
+            } else if (profilePageProps.setCurrentCard) {
+                // Set the current card
+                profilePageProps.setCurrentCard!(card as TradingCardInfo);
+            }
+        }
+    }
 
-	const isMobile = useBreakpointValue({ base: true, md: false });
+    useEffect(() => {
+        if (currentPage > totalPages) {
+            setCurrentPage(1);
+            localStorage.setItem("lastPaginationPage", "1");
+        }
+    }, [currentItems, currentPage, totalPages, setCurrentPage]);
 
-	/**
-	 * Focuses on the current card when the view card button or the card is clicked.
-	 */
-	function focusCurrentCard(card: TradingCardInfo | SerializedTradingCard) {
-		if (profilePageProps && profilePageProps.setViewCardClicked && profilePageProps.currentCard) {
-			// Set the view card clicked to true
-			profilePageProps.setViewCardClicked(true);
-			// If the card is a serialized card, set the current serialized card
-			if (card instanceof SerializedTradingCard && profilePageProps.setCurrentSerializedCard) {
-				profilePageProps.setCurrentSerializedCard(card);
-			} else if (profilePageProps.setCurrentCard) {
-				// Set the current card
-				profilePageProps.setCurrentCard!(card as TradingCardInfo);
-			}
-		}
-	}
+    // Scroll to the top of the page when the page changes
+    useEffect(() => {
+        if (targetRef.current) {
+            // If we are on the first page and didnt navigate backwards, dont scroll
+            if (currentPage === 1 && !isPageFlipping) {
+                return;
+            }
 
-	useEffect(() => {
-		if (currentPage > totalPages) {
-			setCurrentPage(1);
-			localStorage.setItem("lastPaginationPage", "1");
-		}
+            targetRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [currentPage, targetRef, isPageFlipping]);
 
-	}, [ currentItems, currentPage, totalPages, setCurrentPage ]);
-
-	// Scroll to the top of the page when the page changes
-	useEffect(() => {
-		if (targetRef.current) {
-
-			// If we are on the first page and didnt navigate backwards, dont scroll
-			if (currentPage === 1 && !isPageFlipping) {
-				return;
-			}
-
-			targetRef.current.scrollIntoView({ behavior: "smooth" });
-
-		}
-	}, [ currentPage, targetRef, isPageFlipping ]);
-
-	return (
-		<Box
-			minWidth="100%"
-			paddingBottom={24}
-			px={{ base: "23px", md: "0px" }}
-		>
-			<SimpleGrid
-				ref={parentStackRef}
-				columns={{
-					"base": 1,
-					"sm": 1,
-					"md": 2,
-					"lg": 2,
-					"xl": 2,
-					"2xl": 2,
-					"3xl": 3,
-				}}
-				spacing={{ base: "36px", md: "41px" }}
-				flexWrap={"wrap"}
-				alignItems={"top"}
-				minWidth="100%"
-				width={profilePage ? "65vw" : undefined}
-				maxWidth={"1250px"}
-				justifyContent={{ base: "center", md: "start" }} // Center on mobile, start on larger screens
-			>
-				{[ ...currentItems, ...currentItems, currentItems[0] ].map((card, index) => {
-					return (
-						<GridItem key={index} flex={1}>
-							<TrendingCard
-								isPageFlipping={isPageFlipping}
-								passedInCard={card}
-								shouldShowButton={isMobile}
-								overrideCardClick={profilePage ? focusCurrentCard : undefined}
-								onProfilePage={profilePage}
-								tabName={profilePage ? profilePageProps?.tabName : undefined}
-								currentUserId={profilePageProps?.currentUserId}
-								setCurrentCard={profilePageProps?.setCurrentCard}
-								onSendCardModalOpen={profilePageProps?.onSendCardModalOpen}
-								loginModalOpen={profilePageProps?.loginModalOpen}
-								onAddToCollectionModalOpen={profilePageProps?.onAddToCollectionModalOpen}
-								privateView={profilePageProps?.privateView}
-							/>
-						</GridItem>
-					);
-				})}
-			</SimpleGrid>
-			{data.length !== 0 ?
-				<PaginationNavigator currentPage={currentPage} totalPages={totalPages} handleClick={handleClick} /> :
-				<Center pt="100px">
-					<Box w={noCardMessageBoxWidth} h="100px" bg="gray.1000" alignContent={"center"} borderRadius="10px">
-						<Text textAlign="center" color="white.1500" fontFamily="Barlow Condensed" fontSize="20px">No cards to show!</Text>
-					</Box>
-				</Center>
-			}
-		</Box>
-	);
-};
+    return (
+        <Box
+            minWidth="100%"
+            paddingBottom={24}
+            px={{ base: "23px", md: "0px" }}
+        >
+            <SimpleGrid
+                ref={parentStackRef}
+                columns={{
+                    base: 1,
+                    sm: 1,
+                    md: 2,
+                    lg: 2,
+                    xl: 2,
+                    "2xl": 2,
+                    "3xl": 3,
+                }}
+                spacing={{ base: "36px", md: "41px" }}
+                flexWrap={"wrap"}
+                alignItems={"top"}
+                minWidth="100%"
+                width={profilePage ? "65vw" : undefined}
+                maxWidth={"1250px"}
+                justifyContent={{ base: "center", md: "start" }} // Center on mobile, start on larger screens
+            >
+                {currentItems.map((card, index) => {
+                    return (
+                        <GridItem key={index} flex={1}>
+                            <TrendingCard
+                                isPageFlipping={isPageFlipping}
+                                passedInCard={card}
+                                shouldShowButton={isMobile}
+                                overrideCardClick={
+                                    profilePage ? focusCurrentCard : undefined
+                                }
+                                onProfilePage={profilePage}
+                                tabName={
+                                    profilePage
+                                        ? profilePageProps?.tabName
+                                        : undefined
+                                }
+                                currentUserId={profilePageProps?.currentUserId}
+                                setCurrentCard={
+                                    profilePageProps?.setCurrentCard
+                                }
+                                onSendCardModalOpen={
+                                    profilePageProps?.onSendCardModalOpen
+                                }
+                                loginModalOpen={
+                                    profilePageProps?.loginModalOpen
+                                }
+                                onAddToCollectionModalOpen={
+                                    profilePageProps?.onAddToCollectionModalOpen
+                                }
+                                privateView={profilePageProps?.privateView}
+                            />
+                        </GridItem>
+                    );
+                })}
+            </SimpleGrid>
+            {data.length !== 0 ? (
+                <PaginationNavigator
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    handleClick={handleClick}
+                />
+            ) : (
+                <Center pt="100px">
+                    <Box
+                        w={noCardMessageBoxWidth}
+                        h="100px"
+                        bg="gray.1000"
+                        alignContent={"center"}
+                        borderRadius="10px"
+                    >
+                        <Text
+                            textAlign="center"
+                            color="white.1500"
+                            fontFamily="Barlow Condensed"
+                            fontSize="20px"
+                        >
+                            No cards to show!
+                        </Text>
+                    </Box>
+                </Center>
+            )}
+        </Box>
+    );
+}
