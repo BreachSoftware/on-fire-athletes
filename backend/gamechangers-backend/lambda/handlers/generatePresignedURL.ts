99620f9af031860d89a0fa79a311b3c9cf4e9ac7
Matthew Ieyoub <mjieyoub@gmail.com>
2024-10-04 12:53:22 -0500

    feat: stuff

--- backend/gamechangers-backend/lambda/handlers/generatePresignedURL.ts
+++ backend/gamechangers-backend/lambda/handlers/generatePresignedURL.ts
@@ -73,8 +73,11 @@ export const generatePresignedURL: Handler = async (
 		};
 	}
 
-	const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } =
-		process.env;
+	const {
+		ACCESS_KEY_ID: AWS_ACCESS_KEY_ID,
+		SECRET_ACCESS_KEY: AWS_SECRET_ACCESS_KEY,
+		REGION: AWS_REGION,
+	} = process.env;
 
 	const s3 = new S3({
 		region: AWS_REGION,
