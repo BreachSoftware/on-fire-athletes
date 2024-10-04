57818f694287914e4b08f1a259f21d687daf7e78
Colin Hebert <cgabear@gmail.com>
2024-10-04 15:24:59 -0500

    feat: scroll on faq click

--- frontend/components/shared/wrappers/shared-stack.tsx
+++ frontend/components/shared/wrappers/shared-stack.tsx
@@ -1,28 +1,30 @@
-import { Stack, StackProps } from '@chakra-ui/react'
-import React from 'react'
+import React from "react";
+import { Stack, StackProps } from "@chakra-ui/react";
 
 interface Props extends StackProps {
-    row?: boolean
-    fit?: boolean
-    spaced?: boolean
+    row?: boolean;
+    fit?: boolean;
+    spaced?: boolean;
 }
 
-export default function SharedStack({
-    row = false,
-    fit = false,
-    spaced = false,
-    children,
-    ...props
-}: Props) {
-    return (
-        <Stack
-            w={fit ? 'fit-content' : 'full'}
-            justify={spaced && row ? 'space-between' : 'flex-start'}
-            align={row ? 'center' : 'flex-start'}
-            direction={row ? 'row' : 'column'}
-            {...props}
-        >
-            {children}
-        </Stack>
-    )
-}
+const SharedStack = React.forwardRef<HTMLDivElement, Props>(
+    ({ row = false, fit = false, spaced = false, children, ...props }, ref) => {
+        return (
+            <Stack
+                ref={ref}
+                width={fit ? "fit-content" : "full"}
+                justify={spaced && row ? "space-between" : "flex-start"}
+                align={row ? "center" : "flex-start"}
+                direction={row ? "row" : "column"}
+                {...props}
+            >
+                {children}
+            </Stack>
+        );
+    },
+);
+
+// It's a good practice to set a display name for debugging purposes
+SharedStack.displayName = "SharedStack";
+
+export default SharedStack;
