177d5c947783d0e3c5b5d3002c987f1c00444ff4
George Villaume <39601888+gvillaume@users.noreply.github.com>
2024-10-10 12:33:48 -0500

    feat: fix request trade feedback

--- frontend/app/profile/components/addToCollectionModal.tsx
+++ frontend/app/profile/components/addToCollectionModal.tsx
@@ -23,65 +23,80 @@ export function AddToCollectionModal(props: AddToCollectionModalProps) {
      * Sends the request to add the card to the user's collection
      */
     async function requestCardEmail() {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        try {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
-        // Get the profile of the user who generated the card
-        const uuidrequestOptions = {
-            method: "GET",
-            headers: myHeaders,
-        };
+            // Get the profile of the user who generated the card
+            const uuidrequestOptions = {
+                method: "GET",
+                headers: myHeaders,
+            };
 
-        const generatedByProfile = await fetch(
-            `${apiEndpoints.getUser()}?uuid=${props.currentCard.generatedBy}`,
-            uuidrequestOptions,
-        );
-        const generatedByProfileData = await generatedByProfile.json();
-        const generatedByEmail = generatedByProfileData.email;
+            const generatedByProfile = await fetch(
+                `${apiEndpoints.getUser()}?uuid=${props.currentCard.generatedBy}`,
+                uuidrequestOptions,
+            );
+            const generatedByProfileData = await generatedByProfile.json();
+            const generatedByEmail = generatedByProfileData.email;
 
-        const requesterProfile = await fetch(
-            `${apiEndpoints.getUser}?uuid=${props.currentUserId}`,
-            uuidrequestOptions,
-        );
-        const requesterProfileData = await requesterProfile.json();
-        const requesterEmail = requesterProfileData.email;
+            const requesterProfile = await fetch(
+                `${apiEndpoints.getUser()}?uuid=${props.currentUserId}`,
+                uuidrequestOptions,
+            );
+            const requesterProfileData = await requesterProfile.json();
+            const requesterEmail = requesterProfileData.email;
 
-        // Send the request to the creator of the card for thier card to be added to the user's collection
-        const emailHeaders = new Headers();
-        emailHeaders.append("Content-Type", "application/json");
+            // Send the request to the creator of the card for thier card to be added to the user's collection
+            const emailHeaders = new Headers();
+            emailHeaders.append("Content-Type", "application/json");
 
-        const raw = JSON.stringify({
-            toEmail: generatedByEmail,
-            requesterUUID: props.currentUserId,
-            generatedByUUID: props.currentCard.generatedBy,
-            cardUUID: props.currentCard.uuid,
-            cardFirstName: props.currentCard.firstName,
-            cardLastName: props.currentCard.lastName,
-            cardImage: props.currentCard.cardImage,
-            requesterEmail: requesterEmail,
-            recipientFirstName: generatedByProfileData.first_name,
-        });
-
-        const requestOptions = {
-            method: "POST",
-            headers: emailHeaders,
-            body: raw,
-            redirect: "follow" as RequestRedirect,
-        };
-
-        const addToCollectionResponse = await fetch(
-            apiEndpoints.addToCollectionEmail(),
-            requestOptions,
-        );
-        if (addToCollectionResponse.status === 200) {
-            toast({
-                title: "Request Sent",
-                description: "The creator has been notified of your request.",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
+            const raw = JSON.stringify({
+                toEmail: generatedByEmail,
+                requesterUUID: props.currentUserId,
+                generatedByUUID: props.currentCard.generatedBy,
+                cardUUID: props.currentCard.uuid,
+                cardFirstName: props.currentCard.firstName,
+                cardLastName: props.currentCard.lastName,
+                cardImage: props.currentCard.cardImage,
+                requesterEmail: requesterEmail,
+                recipientFirstName: generatedByProfileData.first_name,
             });
-        } else {
+
+            const requestOptions = {
+                method: "POST",
+                headers: emailHeaders,
+                body: raw,
+                redirect: "follow" as RequestRedirect,
+            };
+
+            const addToCollectionResponse = await fetch(
+                apiEndpoints.addToCollectionEmail(),
+                requestOptions,
+            );
+
+            if (addToCollectionResponse.status === 200) {
+                toast({
+                    title: "Request Sent",
+                    description:
+                        "The creator has been notified of your request.",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                props.onClose();
+            } else {
+                toast({
+                    title: "Request Failed",
+                    description:
+                        "An error occurred while sending your request. Please try again later.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        } catch (error) {
+            console.error("Error requesting card email", error);
             toast({
                 title: "Request Failed",
                 description:
@@ -91,8 +106,6 @@ export function AddToCollectionModal(props: AddToCollectionModalProps) {
                 isClosable: true,
             });
         }
-
-        props.onClose();
     }
 
     return (
